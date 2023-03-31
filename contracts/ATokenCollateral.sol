cc9c214edc9cff0c823f5033d4eade3f11ee6c1b
avinash-n992 <avinash.nair6@gmail.com>
2023-03-31 11:56:37 +0530

    initial part - base contracts of reserve added

---
+++ contracts/ATokenCollateral.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+import './IAsset.sol';
+
+contract AaveCollateral is ICollateral{
+
+    function bal(address acc) external view returns(uint192){
+        
+    }
+}
